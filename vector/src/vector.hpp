cab24b6d1e2647f9bd647d5fcd1fa8ba40983b07
pigstd <zhuzemu@sjtu.edu.cn>
2025-02-25 08:14:32 +0800

    完成 vector.hpp

--- vector/src/vector.hpp
+++ vector/src/vector.hpp
@@ -5,6 +5,8 @@
 
 #include <climits>
 #include <cstddef>
+#include <cstring>
+#include <iostream>
 
 namespace sjtu
 {
@@ -15,6 +17,21 @@ namespace sjtu
 template<typename T>
 class vector
 {
+private:
+	void *MemoryData;
+	int Size, Capability;
+	// 扩大 Capability
+	void extent() {
+		int newCapability = Capability * 2;
+		if (Capability == 0) newCapability = 1;
+		void *newMemoryData = operator new (newCapability * sizeof(T));
+		for (int i = 0; i < Size; i++)
+			new(static_cast<T*>(newMemoryData) + i) T(*(static_cast<T*>(MemoryData) + i)),
+			(static_cast<T*>(MemoryData) + i)->~T();
+		if (MemoryData != nullptr) operator delete(MemoryData);
+		MemoryData = newMemoryData;
+		Capability = newCapability;
+	}
 public:
 	/**
 	 * TODO
@@ -24,7 +41,78 @@ public:
 	/**
 	 * you can see RandomAccessIterator at CppReference for help.
 	 */
-	class const_iterator;
+	/**
+	 * TODO
+	 * has same function as iterator, just for a const object.
+	 */
+	 class const_iterator
+	 {
+	 public:
+		 using difference_type = std::ptrdiff_t;
+		 using value_type = T;
+		 using pointer = T*;
+		 using reference = T&;
+		 using iterator_category = std::output_iterator_tag;
+ 
+	 private:
+		 void *MemoryData;
+		 int index;
+	 public:
+		 const_iterator(void *_MemoryData = nullptr, int index_ = 0) : MemoryData(_MemoryData), index(index_) {}
+		 const_iterator operator+(const int &n) const {
+			 return {MemoryData, index + n};
+		 }
+		 const_iterator operator-(const int &n) const {
+			 return {MemoryData, index - n};
+		 }
+		 // return the distance between two iterators,
+		 // if these two iterators point to different vectors, throw invaild_iterator.
+		 int operator-(const const_iterator &rhs) const {
+			 if (rhs.MemoryData != MemoryData) throw invalid_iterator();
+			 return index - rhs.index;
+		 }
+		 const_iterator& operator+=(const int &n) {
+			 index += n;
+			 return *this;
+		 }
+		 const_iterator& operator-=(const int &n) {
+			 index -= n;
+			 return *this;
+		 }
+		 const_iterator operator++(int) {
+			 const_iterator tmp = *this;
+			 ++index;
+			 return tmp;
+		 }
+		 const_iterator& operator++() {
+			 ++index;
+			 return *this;
+		 }
+		 const_iterator operator--(int) {
+			 iterator tmp = *this;
+			 --index;
+			 return tmp;
+		 }
+		 const_iterator& operator--() {
+			 --index;
+			 return *this;
+		 }
+		 const T& operator*() const {
+			 return *(static_cast<T*>(MemoryData) + index);
+		 }
+		 /**
+		  * a operator to check whether two iterators are same (pointing to the same memory address).
+		  */
+		 bool operator==(const const_iterator &rhs) const {
+			 return MemoryData == rhs.MemoryData && index == rhs.index;
+		 }
+		 /**
+		  * some other operator for iterator.
+		  */
+		 bool operator!=(const const_iterator &rhs) const {
+			 return MemoryData != rhs.MemoryData || index != rhs.index;
+		 }
+	 };
 	class iterator
 	{
 	// The following code is written for the C++ type_traits library.
@@ -46,180 +134,251 @@ public:
 		using iterator_category = std::output_iterator_tag;
 
 	private:
-		/**
-		 * TODO add data members
-		 *   just add whatever you want.
-		 */
+		void *MemoryData;
+		int index;
 	public:
+		iterator(void *_MemoryData = nullptr, int index_ = 0) : MemoryData(_MemoryData), index(index_) {}
 		/**
 		 * return a new iterator which pointer n-next elements
 		 * as well as operator-
 		 */
-		iterator operator+(const int &n) const
-		{
-			//TODO
+		iterator operator+(const int &n) const {
+			return {MemoryData, index + n};
 		}
-		iterator operator-(const int &n) const
-		{
-			//TODO
+		iterator operator-(const int &n) const {
+			return {MemoryData, index - n};
 		}
 		// return the distance between two iterators,
 		// if these two iterators point to different vectors, throw invaild_iterator.
-		int operator-(const iterator &rhs) const
-		{
-			//TODO
+		int operator-(const iterator &rhs) const {
+			if (rhs.MemoryData != MemoryData) throw invalid_iterator();
+			return index - rhs.index;
 		}
-		iterator& operator+=(const int &n)
-		{
-			//TODO
+		iterator& operator+=(const int &n) {
+			index += n;
+			return *this;
 		}
-		iterator& operator-=(const int &n)
-		{
-			//TODO
+		iterator& operator-=(const int &n) {
+			index -= n;
+			return *this;
 		}
-		/**
-		 * TODO iter++
-		 */
-		iterator operator++(int) {}
-		/**
-		 * TODO ++iter
-		 */
-		iterator& operator++() {}
-		/**
-		 * TODO iter--
-		 */
-		iterator operator--(int) {}
+		iterator operator++(int) {
+			iterator tmp = *this;
+			++index;
+			return tmp;
+		}
+		iterator& operator++() {
+			++index;
+			return *this;
+		}
+		iterator operator--(int) {
+			iterator tmp = *this;
+			--index;
+			return tmp;
+		}
 		/**
 		 * TODO --iter
 		 */
-		iterator& operator--() {}
+		iterator& operator--() {
+			--index;
+			return *this;
+		}
 		/**
 		 * TODO *it
 		 */
-		T& operator*() const{}
+		T& operator*() const{
+			return *(static_cast<T*>(MemoryData) + index);
+		}
 		/**
 		 * a operator to check whether two iterators are same (pointing to the same memory address).
 		 */
-		bool operator==(const iterator &rhs) const {}
-		bool operator==(const const_iterator &rhs) const {}
+		bool operator==(const iterator &rhs) const {
+			return MemoryData == rhs.MemoryData && index == rhs.index;
+		}
+		// bool operator==(const const_iterator &rhs) const;
 		/**
 		 * some other operator for iterator.
 		 */
-		bool operator!=(const iterator &rhs) const {}
-		bool operator!=(const const_iterator &rhs) const {}
-	};
-	/**
-	 * TODO
-	 * has same function as iterator, just for a const object.
-	 */
-	class const_iterator
-	{
-	public:
-		using difference_type = std::ptrdiff_t;
-		using value_type = T;
-		using pointer = T*;
-		using reference = T&;
-		using iterator_category = std::output_iterator_tag;
+		bool operator!=(const iterator &rhs) const {
+			return MemoryData != rhs.MemoryData || index != rhs.index;
+		}
+		// bool operator!=(const const_iterator &rhs) const;
 
-	private:
-		/*TODO*/
-
+		// 只需要支持隐式转换 const_iterator，就不需要上面那两个东西了
+		// 隐式转换为 const_iterator
+		operator const_iterator() {
+			return const_iterator(MemoryData, index);
+		}
 	};
 	/**
 	 * TODO Constructs
 	 * At least two: default constructor, copy constructor
 	 */
-	vector() {}
-	vector(const vector &other) {}
+	vector() : Size(0), Capability(0), MemoryData(nullptr) {}
+	vector(const vector &other) : Size(other.Size), Capability(other.Size), MemoryData(nullptr) {
+		MemoryData = operator new (Capability * sizeof(T));
+		for (int i = 0; i < Size; i++)
+			new (static_cast<T*>(MemoryData) + i) T(other[i]);
+	}
 	/**
 	 * TODO Destructor
 	 */
-	~vector() {}
+	~vector() {
+		for (int i = 0; i < Size; i++)
+			this->operator[](i).~T();
+		operator delete(MemoryData);
+	}
 	/**
 	 * TODO Assignment operator
 	 */
-	vector &operator=(const vector &other) {}
+	vector &operator=(const vector &other) {
+		if (this == &other) return *this;
+		
+		for (int i = 0; i < Size; i++)
+			this->operator[](i).~T();
+		operator delete(MemoryData);
+
+		Size = other.Size, Capability = other.Size;
+		MemoryData = operator new(sizeof(T) * Capability);
+		for (int i = 0; i < Size; i++)
+			new (static_cast<T*>(MemoryData) + i) T(other[i]);
+		return *this;
+	}
 	/**
 	 * assigns specified element with bounds checking
 	 * throw index_out_of_bound if pos is not in [0, size)
 	 */
-	T & at(const size_t &pos) {}
-	const T & at(const size_t &pos) const {}
+	T & at(const size_t &pos) {
+		if (pos < 0 || pos >= Size) throw index_out_of_bound();
+		return *(static_cast<T*>(MemoryData) + pos);
+	}
+	const T & at(const size_t &pos) const {
+		if (pos < 0 || pos >= Size) throw index_out_of_bound();
+		return *(static_cast<T*>(MemoryData) + pos);
+	}
 	/**
 	 * assigns specified element with bounds checking
 	 * throw index_out_of_bound if pos is not in [0, size)
 	 * !!! Pay attentions
 	 *   In STL this operator does not check the boundary but I want you to do.
 	 */
-	T & operator[](const size_t &pos) {}
-	const T & operator[](const size_t &pos) const {}
+	T & operator[](const size_t &pos) {
+		return at(pos);
+	}
+	const T & operator[](const size_t &pos) const {
+		return at(pos);
+	}
 	/**
 	 * access the first element.
 	 * throw container_is_empty if size == 0
 	 */
-	const T & front() const {}
+	const T & front() const {
+		if (Size == 0) throw container_is_empty();
+		return at(0);
+	}
 	/**
 	 * access the last element.
 	 * throw container_is_empty if size == 0
 	 */
-	const T & back() const {}
+	const T & back() const {
+		if (Size == 0) throw container_is_empty();
+		return at(Size - 1);
+	}
 	/**
 	 * returns an iterator to the beginning.
 	 */
-	iterator begin() {}
-	const_iterator begin() const {}
-	const_iterator cbegin() const {}
+	iterator begin() {return iterator(MemoryData, 0);}
+	const_iterator begin() const {return const_iterator(MemoryData, 0);}
+	const_iterator cbegin() const {return const_iterator(MemoryData, 0);}
 	/**
 	 * returns an iterator to the end.
 	 */
-	iterator end() {}
-	const_iterator end() const {}
-	const_iterator cend() const {}
+	iterator end() {return iterator(MemoryData, Size);}
+	const_iterator end() const {return const_iterator(MemoryData, Size);}
+	const_iterator cend() const {return const_iterator(MemoryData, Size);}
 	/**
 	 * checks whether the container is empty
 	 */
-	bool empty() const {}
+	bool empty() const {return Size == 0;}
 	/**
 	 * returns the number of elements
 	 */
-	size_t size() const {}
+	size_t size() const {return Size;}
 	/**
 	 * clears the contents
 	 */
-	void clear() {}
-	/**
-	 * inserts value before pos
-	 * returns an iterator pointing to the inserted value.
-	 */
-	iterator insert(iterator pos, const T &value) {}
+	void clear() {
+		for (int i = 0; i < Size; i++)
+			this->operator[](i).~T();
+		operator delete(MemoryData);
+
+		MemoryData = nullptr;
+		Size = Capability = 0;
+	}
 	/**
 	 * inserts value at index ind.
 	 * after inserting, this->at(ind) == value
 	 * returns an iterator pointing to the inserted value.
 	 * throw index_out_of_bound if ind > size (in this situation ind can be size because after inserting the size will increase 1.)
 	 */
-	iterator insert(const size_t &ind, const T &value) {}
+	iterator insert(const size_t &ind, const T &value) {
+		// std::cerr << "ins: " << ind << ' ' << Size << '\n';
+		if (ind > Size) throw index_out_of_bound();
+		if (Size == Capability) extent();
+		new(static_cast<T*>(MemoryData) + Size) T(at(Size - 1));
+		for (int i = Size - 1; i > ind; i--)
+			at(i) = at(i - 1);
+		Size++;
+		at(ind) = value;
+		return iterator(MemoryData, ind);
+	}
 	/**
-	 * removes the element at pos.
-	 * return an iterator pointing to the following element.
-	 * If the iterator pos refers the last element, the end() iterator is returned.
+	 * inserts value before pos
+	 * returns an iterator pointing to the inserted value.
 	 */
-	iterator erase(iterator pos) {}
+	iterator insert(iterator pos, const T &value) {
+		int index = pos - begin();
+		return insert(index, value);
+	}
+	
 	/**
 	 * removes the element with index ind.
 	 * return an iterator pointing to the following element.
 	 * throw index_out_of_bound if ind >= size
 	 */
-	iterator erase(const size_t &ind) {}
+	iterator erase(const size_t &ind) {
+		if (ind >= Size) throw index_out_of_bound();
+		at(ind).~T();
+		for (int i = ind; i + 1 < Size; i++)
+			at(i) = at(i + 1);
+		Size--;
+		return begin() + ind;
+	}
+	/**
+	 * removes the element at pos.
+	 * return an iterator pointing to the following element.
+	 * If the iterator pos refers the last element, the end() iterator is returned.
+	 */
+	iterator erase(iterator pos) {
+		return erase(pos - begin());
+	}
 	/**
 	 * adds an element to the end.
 	 */
-	void push_back(const T &value) {}
+	void push_back(const T &value) {
+		if (Size == Capability) extent();
+		new (static_cast<T*>(MemoryData) + Size) T(value);
+		Size++;
+	}
 	/**
 	 * remove the last element from the end.
 	 * throw container_is_empty if size() == 0
 	 */
-	void pop_back() {}
+	void pop_back() {
+		if (Size == 0) throw container_is_empty();
+		at(Size - 1).~T();
+		Size--;
+	}
 };
 
 
